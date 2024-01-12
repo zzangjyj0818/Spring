b41727b53cde10f27ba910bbbf95d5de3e4f1f4e
zzangjyj0818 <jyj010818@naver.com>
2024-01-12 13:33:54 +0900

    fix error

--- SpringInAction/Chap02/taco-cloud/src/main/java/tacos/Taco.java
+++ SpringInAction/Chap02/taco-cloud/src/main/java/tacos/Taco.java
@@ -9,8 +9,8 @@ import java.util.List;
 @Data
 public class Taco {
     @NotNull
-    @Size(min=5, message = "Name must be at least 5 characters long")
+    @Size(min=5,message="Name must be at least 5 characters long")
     private String name;
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @Size(min=1,message="You must choose at least 1 ingredient")
     private List<String> ingredients;
 }
